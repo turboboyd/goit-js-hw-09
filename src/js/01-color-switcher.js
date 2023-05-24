06906fa31df3315a7c325bd7ac8ee968c91944ac
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-24 22:07:23 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const btnStart = document.querySelector(`button[data-start]`);
+const btnStop = document.querySelector(`button[data-stop]`);
+const bodyEl = document.querySelector('body');
+
+let timerId = null;
+let isActivBtn = false;
+
+btnStart.addEventListener('click', () => {
+  if (isActivBtn) {
+    return;
+  }
+  timerId = setInterval(() => {
+    isActivBtn = true;
+      console.log('fskfk');
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+      
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+    isActivBtn = false;
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+  console.log('safasf');
+});
