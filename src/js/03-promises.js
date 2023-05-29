22a0c053af22e038923c19e5923505d56d02ae76
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-29 11:31:59 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+document.querySelector('.form').addEventListener('submit', submitBtn);
+
+function submitBtn(e) {
+  e.preventDefault();
+  const valueDelay = document.querySelector('input[name="delay"]').value;
+  const valueStep = document.querySelector('input[name="step"]').value;
+  const valueAmount = document.querySelector('input[name="amount"]').value;
+  for (let index = 0; index < valueAmount; index++) {
+    createPromise(valueDelay, valueStep)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
