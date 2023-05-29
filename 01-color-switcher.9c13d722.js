0e95e4716d85f22c55bcfe414486994f3a5475b4
turboboyd <113061164+turboboyd@users.noreply.github.com>
2023-05-29 10:39:55 +0000

    Deploying to gh-pages from @ turboboyd/goit-js-hw-09@67818956334a5a6ba7320a1fc75846f1927375d4 🚀

--- 01-color-switcher.9c13d722.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),a=null;e.disabled=!0,t.addEventListener("click",(function(){a=setInterval((function(){t.disabled=!0,e.disabled=!1,n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.9c13d722.js.map
